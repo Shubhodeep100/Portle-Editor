7ece2692107beb204b0a9f36d21d6da44ae066e5
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-23 21:04:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "draft-js/dist/Draft.css";
+import DraftEditor from "./components/DraftEditor";
+
+
+function App() {
+  return (
+    <div className="App">
+      <DraftEditor />
+    </div>
+  );
+}
+
+export default App;
